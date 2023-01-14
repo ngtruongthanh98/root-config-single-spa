48742c14a61fe48cbe3b114c68c225acf77ab4f5
ThanhNT26 <ngtruongthanh98@gmail.com>
2023-01-14 16:44:40 +0700

    feat: Setup Single SPA root file webpack

---
+++ src/mfe-root-config.js
@@ -0,0 +1,21 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@mfe/react-app",
+  app: () => System.import("@mfe/react-app"),
+  activeWhen: ["/react"],
+});
+
+// registerApplication({
+//   name: "@mfe/vue-app",
+//   app: () => System.import("@mfe/vue-app"),
+//   activeWhen: ["/vue"],
+// });
+
+// registerApplication({
+//   name: "@mfe/react-navbar",
+//   app: () => System.import("@mfe/react-navbar"),
+//   activeWhen: ["/"],
+// });
+
+start();
---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, "./dist"),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 9000,
+  },
+
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
